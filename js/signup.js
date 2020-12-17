9d076e528b252597088d198e1407db1a963637ce
dragosmocanasu <stilldragos10@gmail.com>
2020-12-17 08:37:16 +0100

    deleted redundant script

--- js/signup.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // Cancel the user's signup process
-    $('input#signupCancelButton').on('click', function() {
-        window.location.replace('login.php');
-    });
-});
